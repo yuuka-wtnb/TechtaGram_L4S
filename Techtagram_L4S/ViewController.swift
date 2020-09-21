a71f4bf0b083d2e172a380f12f1679d30761283b
Yuka Watanabe <yuuka.wtnb.011@gmail.com>
2020-09-21 22:36:30 +0900

    Initial Commit

---
+++ Techtagram_L4S/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techtagram_L4S
+//
+//  Created by Yuuka Watanabe on 2020/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
